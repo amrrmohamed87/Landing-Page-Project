a0753bf971e80746db29ae210011c2b242a1c377
amrrmohamed87 <113718856+amrrmohamed87@users.noreply.github.com>
2022-10-24 09:06:30 +0200

    Add files via upload

---
+++ js/landingPage.js
@@ -0,0 +1,27 @@
+//getting access to anchor points by using -> querySelectorAll() then we will 
+//store them inside an array so we can add eventlistener to all of them at the same time
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    //adding eventlistener works by click
+    anchor.addEventListener("click", function(e){
+        /*prevent the default to basically take scroll
+        smoothly to the section, not jumping over the
+        section*/
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+//active class
+window.onscroll = function(){
+    document.querySelectorAll("section").forEach(function(active){
+        if(
+            active.getBoundingClientRect().top >= -300 &&
+            active.getBoundingClientRect().top <= 150
+        ){
+            active.classList.add("your-active-class");
+        }else{
+            active.classList.remove("your-active-class");
+        }
+    });
+};
